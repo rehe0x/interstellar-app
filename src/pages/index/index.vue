ca4bea711f651f2316fcd14dc5c1d320083062d2
[xieh1995] <[74551006@qq.com]>
2021-07-23 17:57:41 +0800

    资源自增加

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -53,8 +53,12 @@ export default {
         scrollTop: 0
       },
       resources: {},
-      buildings: []
+      buildings: [],
+      count: 0
     }
+  },
+  onLoad () {
+
   },
   async mounted () {
     const resource = await getResources()
@@ -62,6 +66,45 @@ export default {
     this.resources = resource.result
     const building = await getBuilding()
     this.buildings = building.result
+
+    let metalT = 0
+    let crystalT = 0
+    let deuteriumT = 0
+    this.resourceTimer = setInterval(() => {
+      if (this.resources.metal < this.resources.metalStorageMax) {
+        if (this.resources.metalTime >= 1) {
+          this.resources.metal = Math.floor(+this.resources.metal + +this.resources.metalTime)
+        } else {
+          metalT += +this.resources.metalTime
+          if (metalT >= 1) {
+            this.resources.metal = Math.floor(+this.resources.metal + +metalT)
+            metalT = 0
+          }
+        }
+      }
+      if (this.resources.crystal < this.resources.crystalStorageMax) {
+        if (this.resources.crystalTime >= 1) {
+          this.resources.crystal = Math.floor(+this.resources.crystal + +this.resources.crystalTime)
+        } else {
+          crystalT += +this.resources.crystalTime
+          if (crystalT >= 1) {
+            this.resources.crystal = Math.floor(+this.resources.crystal + +crystalT)
+            crystalT = crystalT % 1
+          }
+        }
+      }
+      if (this.resources.deuterium < this.resources.deuteriumStorageMax) {
+        if (this.resources.deuteriumTime >= 1) {
+          this.resources.deuterium = Math.floor(+this.resources.deuterium + +this.resources.deuteriumTime)
+        } else {
+          deuteriumT += +this.resources.deuteriumTime
+          if (deuteriumT >= 1) {
+            this.resources.deuterium = Math.floor(+this.resources.deuterium + +deuteriumT)
+            deuteriumT = 0
+          }
+        }
+      }
+    }, 1000)
   },
   methods: {
     upper: function (e) {
@@ -83,6 +126,9 @@ export default {
         icon: 'loading',
         duration: 1500
       })
+    },
+    destroyed () {
+      clearInterval(this.resourceTimer)
     }
   }
 }
